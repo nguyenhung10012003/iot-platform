5b65207065f58f6c332400cc7a49cec83db3b0da
milo <nguyenhung1012003@gmail.com>
2024-10-24 15:36:52 +0700

    Refactor image source in AuthLayout component

--- apps/web/app/[lang]/(auth)/layout.tsx
+++ apps/web/app/[lang]/(auth)/layout.tsx
@@ -11,11 +11,12 @@ export default function AuthLayout({
       <Card className="shadow-lg flex flex-row w-full md:max-w-[800px] max-w-md overflow-hidden">
         <div className="flex-1 hidden md:flex">
           <Image
-            src="/agri.jpg"
+            src="https://iotplatf.s3.ap-southeast-1.amazonaws.com/agri.jpg"
             width={500}
             height={500}
             alt=""
             className="object-cover flex w-full h-full"
+            priority
           />
         </div>
         <div className="flex-1">{children}</div>
