2206194809de51ab17c5566146a71dfcaae548b9
Nguyễn Đức Hùng <71268621+nguyenhung10012003@users.noreply.github.com>
2025-03-09 09:24:45 +0000

    Refactor irrigation service to remove unused code and commented out code

--- apps/api/src/irrigation/irrigation.service.ts
+++ apps/api/src/irrigation/irrigation.service.ts
@@ -14,77 +14,77 @@ interface LatestData {
 }
 
 @Injectable()
-export class IrrigationService implements OnModuleInit {
+export class IrrigationService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly predictService: PredictService,
     private readonly scheduler: SchedulerService,
   ) {}
-  async onModuleInit() {
-    this.scheduler.addCronJob(
-      'Check_Irrigation',
-      CronExpression.EVERY_30_MINUTES,
-      async () => {
-        const areas = await this.prisma.area.findMany({
-          include: { location: true },
-        });
-        areas.map(async (area) => {
-          const devices = await this.prisma.device.findMany({
-            where: {
-              areaId: area.id,
-              deviceType: 'SENSOR',
-            },
-          });
-          const latestData: LatestData = {};
-          const currentTime = Date.now();
-          devices.forEach((device) => {
-            device.data.forEach((data) => {
-              const key = (data.type.charAt(0).toLowerCase() +
-                data.type.slice(1)) as keyof LatestData;
-              const currentLatestTime =
-                latestData[key] !== undefined ? latestData[key] : -Infinity;
+  // async onModuleInit() {
+  //   this.scheduler.addCronJob(
+  //     'Check_Irrigation',
+  //     CronExpression.EVERY_30_MINUTES,
+  //     async () => {
+  //       const areas = await this.prisma.area.findMany({
+  //         include: { location: true },
+  //       });
+  //       areas.map(async (area) => {
+  //         const devices = await this.prisma.device.findMany({
+  //           where: {
+  //             areaId: area.id,
+  //             deviceType: 'SENSOR',
+  //           },
+  //         });
+  //         const latestData: LatestData = {};
+  //         const currentTime = Date.now();
+  //         devices.forEach((device) => {
+  //           device.data.forEach((data) => {
+  //             const key = (data.type.charAt(0).toLowerCase() +
+  //               data.type.slice(1)) as keyof LatestData;
+  //             const currentLatestTime =
+  //               latestData[key] !== undefined ? latestData[key] : -Infinity;
 
-              if (
-                data.time > currentLatestTime &&
-                currentTime - data.time <= 3600 * 1000
-              ) {
-                latestData[key] = data.data;
-              }
-            });
-          });
-          Logger.debug(
-            'Latest data: ' + JSON.stringify(latestData),
-            IrrigationService.name,
-          );
+  //             if (
+  //               data.time > currentLatestTime &&
+  //               currentTime - data.time <= 3600 * 1000
+  //             ) {
+  //               latestData[key] = data.data;
+  //             }
+  //           });
+  //         });
+  //         Logger.debug(
+  //           'Latest data: ' + JSON.stringify(latestData),
+  //           IrrigationService.name,
+  //         );
 
-          const predictResult = await this.predictService.predict(latestData);
-          const irrigationSpeed =
-            area.location?.setting?.dripRatePerHole *
-            area.location?.setting?.totalHole;
+  //         const predictResult = await this.predictService.predict(latestData);
+  //         const irrigationSpeed =
+  //           area.location?.setting?.dripRatePerHole *
+  //           area.location?.setting?.totalHole;
 
-          Logger.debug(
-            'Predict result: ' + JSON.stringify(predictResult),
-            IrrigationService.name,
-          );
-          if (predictResult?.prediction && irrigationSpeed) {
-            const amount = predictResult.prediction * 1000;
-            const time = amount / irrigationSpeed;
-            await this.prisma.irrigation.create({
-              data: {
-                amount: amount,
-                locationId: area.locationId,
-                time: time,
-              },
-            });
-            Logger.debug(
-              'Irrigation scheduled for location: ' + area.locationId,
-              IrrigationService.name,
-            );
-          }
-        });
-      },
-    );
-  }
+  //         Logger.debug(
+  //           'Predict result: ' + JSON.stringify(predictResult),
+  //           IrrigationService.name,
+  //         );
+  //         if (predictResult?.prediction && irrigationSpeed) {
+  //           const amount = predictResult.prediction * 1000;
+  //           const time = amount / irrigationSpeed;
+  //           await this.prisma.irrigation.create({
+  //             data: {
+  //               amount: amount,
+  //               locationId: area.locationId,
+  //               time: time,
+  //             },
+  //           });
+  //           Logger.debug(
+  //             'Irrigation scheduled for location: ' + area.locationId,
+  //             IrrigationService.name,
+  //           );
+  //         }
+  //       });
+  //     },
+  //   );
+  // }
 
   async getIrrigations(args: Prisma.IrrigationFindManyArgs) {
     return this.prisma.irrigation.findMany(args);
